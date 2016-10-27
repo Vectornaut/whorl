14d1423d7266985ac5fdc7f76131d524b49c62d3
Aaron Fenyes <afenyes@math.toronto.edu>
2016-10-27 17:43:56 -0400

    Add numerical abelianization routine for rectangles

--- rectangle.jl
+++ rectangle.jl
@@ -37,6 +37,26 @@ function cocycle{R <: AbstractInterval}(angle::R, loc::RectangleLocSys{R})
   Cocycle(cumsum(measure), f_transit, [2, 1])
 end
 
+function abelianize{R <: AbstractInterval}(angle::R, loc::RectangleLocSys, depth::Integer)
+  # build cocycle
+  orig = cocycle(angle, loc)
+  
+  # evolve cocycle
+  iter = orig
+  for _ in 1:depth
+    iter = twostep(iter)
+  end
+  
+  # abelianize
+  ab = IntervalExchange.abelianize(orig, iter)
+  ab_transit = [block.f_transit for block in ab.blocks_by_in]
+  if (angle < @interval(pi)/2)
+    RectangleLocSys{AbstractInterval}(loc.width, loc.height, inv(ab_transit[2]), ab_transit[1])
+  else
+    RectangleLocSys{AbstractInterval}(loc.width, loc.height, ab_transit[1], ab_transit[2])
+  end
+end
+
 # this approximation is accurate for angles very close to pi/2
 function appx_abelianize{R <: AbstractInterval}(tilt_sgn, loc::RectangleLocSys{R})
   # go into a basis where the north transition map is diagonal
@@ -45,7 +65,7 @@ function appx_abelianize{R <: AbstractInterval}(tilt_sgn, loc::RectangleLocSys{R
     λ = n_eigvals[1]
   else
     λ = n_eigvals[2]
-    n_eigframe = [n_eigframe[2] n_eigframe[1]]
+    n_eigframe = [n_eigframe[:,2] n_eigframe[:,1]]
   end
   w_reframed = inv(n_eigframe) * loc.w_transit * n_eigframe
   
